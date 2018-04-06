5f0085e57484156048cf53283e0c9423195898c2
zzuchenyb@sina.com <chen13559469>
2018-04-06 21:47:54 +0800

    更新

--- src/main/java/com/cyb/es/dao/NewsSearchRepositoryES.java
+++
@@ -1,9 +0,0 @@
-package com.cyb.es.dao;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.cyb.es.document.News;
-
-public interface NewsSearchRepositoryES extends ElasticsearchRepository<News, Long>{
-
-}
